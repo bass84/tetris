5ce8349fc638795a7f62e66392073aed3cefc7d5
PaKtitucci <PaKtitucci@218.38.137.28>
2017-01-25 00:30:09 +0900

    2017-01-25

--- tetris/src/main/MainController.java
+++ tetris/src/main/MainController.java
@@ -9,11 +9,10 @@ public class MainController extends PApplet{
 
 	private Background back;
 	private Shape shape;
-	public final static int backgroundWidth = 500;
-	public final static int backgroundHeight = 700;
-	public final static int blockLength = 25;
-	//private int moveHeight = 25;
-	public static boolean[][] isUsed = new boolean[(backgroundWidth / blockLength) + 1][(backgroundHeight / blockLength) + 1];
+	public final static int backgroundWidth = 500;	//너비
+	public final static int backgroundHeight = 700;	//깊이
+	public final static int blockLength = 25;	//블럭 한개 크기
+	public static boolean[][] isUsed = new boolean[(backgroundWidth / blockLength) + 1][(backgroundHeight / blockLength) + 1];	// 각 블럭 boolean값
 	private List<Shape> shapeList = new ArrayList<Shape>();
 	
 	public static void main(String[] args) {
@@ -21,12 +20,12 @@ public class MainController extends PApplet{
     }
 
 	public void settings(){
-		size(this.backgroundWidth, this.backgroundHeight);
+		size(this.backgroundWidth, this.backgroundHeight);	//전체 크기 설정
     }
 	
 	public void setup(){
 		back = new Background(this);
-		shape = new Shape(this);
+		shape = new Shape(this);	//도형 생성
 		
 		for(int i = 1; i < (this.backgroundWidth / this.blockLength) + 1; i++){	//	가장 아랫줄 true
 			isUsed[i][this.backgroundHeight / this.blockLength] = true;
@@ -36,23 +35,24 @@ public class MainController extends PApplet{
 		}
 	}
 	
-	public void draw() {
+	public void draw() {	// 각 도형의 움직임을 그린다.
 		back.init(backgroundWidth, backgroundHeight);
-		
+		this.drawShapeList();
 		
 		if(shape.drawShape(Direction.nothing)) {
 			shapeList.add(shape);
 			shape = new Shape(this);
-		};
-		if(shapeList.size() > 0) {
-			this.drawShapeList();
 		}
 		
 	}
 	
-	private void drawShapeList() {
+	private void drawShapeList() {	// 각 도형들을 내려진 자리에 그린다.
+		if(this.shapeList.size() == 0) return;
+		
 		for(int i = 0; i < this.shapeList.size(); i++) {
-			this.shapeList.get(i).drawShape(Direction.nothing);
+			Shape shape = this.shapeList.get(i);
+			fill(Integer.parseInt(shape.getRgb()[0].trim()), Integer.parseInt(shape.getRgb()[1].trim()), Integer.parseInt(shape.getRgb()[2].trim()));	//도형 색상 지정
+			rect(shape.getWidthLocation(), shape.getHeightLocation(), shape.getSizeX(), shape.getSizeY());	// 도형 그리기
 		}
 	}
 
--- tetris/src/main/Shape.java
+++ tetris/src/main/Shape.java
@@ -14,7 +14,7 @@ public class Shape extends MainController{
 	private ShapeMapping mapping;
 	private String[] param;	// 도형에 대한 파라미터
 	private PApplet pApplet;
-	private String[] rgb = new String[3];	// 도형색상
+	private String[] rgb;	// 도형색상
 	private int widthLocation;	//좌우 위치
 	private int heightLocation;	//높이 위치
 	private int downTerm = 60;	//내려오는 속도
@@ -30,52 +30,79 @@ public class Shape extends MainController{
 		this.heightLocation = Integer.parseInt(this.param[4].trim());
 		this.sizeX = Integer.parseInt(this.param[5].trim());
 		this.sizeY = Integer.parseInt(this.param[6].trim());
+		this.rgb = new String[3];
+		for(int i = 0; i < this.rgb.length; i++) {
+			this.rgb[i] = param[i];
+		}
+		//System.arraycopy(param, 0, rgb, 0, 3);
+	}
+	
+	public int getWidthLocation() {
+		return this.widthLocation;
+	}
+	public int getHeightLocation() {
+		return this.heightLocation;
+	}
+	public int getSizeX() {
+		return this.sizeX;
+	}
+	public int getSizeY() {
+		return this.sizeY;
+	}
+	public String[] getRgb() {
+		return this.rgb;
 	}
 	
 	
 	public boolean drawShape(Direction direction) {
-		System.arraycopy(param, 0, rgb, 0, 3);
-		pApplet.fill(Integer.parseInt(rgb[0].trim()), Integer.parseInt(rgb[1].trim()), Integer.parseInt(rgb[2].trim()));	//도형 색상 지정
+		boolean isCannotDown  = this.fallDownShape(direction) ?  true : false;
 		
-		boolean isCannotDown = false;
-		isCannotDown = this.fallDownShape(direction) == true ?  true : false;
-		//this.changeShape();
-		
-		pApplet.rect(this.widthLocation, this.heightLocation, this.sizeX, this.sizeY);
+		if(!isCannotDown){
+			pApplet.fill(Integer.parseInt(rgb[0].trim()), Integer.parseInt(rgb[1].trim()), Integer.parseInt(rgb[2].trim()));	//도형 색상 지정
+			pApplet.rect(this.widthLocation, this.heightLocation, this.sizeX, this.sizeY);
+		}
 		
 		return isCannotDown;
 	}
 
 
 	private boolean fallDownShape(Direction direction) {
-		if(this.getUsedShape() == Direction.cannotDown) {
-			System.out.println("cannotDown");
-			return true;
-		}
-		else if(this.getUsedShape() == Direction.nothing) this.widthLocation += (direction == Direction.left ? (MainController.blockLength) * -1 : direction == Direction.right ? (MainController.blockLength) : 0);
-		else if(this.getUsedShape() == Direction.cannotLeft) this.widthLocation += (direction == Direction.right ? (MainController.blockLength) : 0);
-		else if(this.getUsedShape() == Direction.cannotRight) this.widthLocation += (direction == Direction.left ? (MainController.blockLength) * -1 : 0);
+		if(this.getIsDown()) return true;
 		
+		this.moveWidth(direction);
 		this.heightLocation += (direction == Direction.down ? MainController.blockLength : 0);
 		if(pApplet.frameCount % this.downTerm == 0) this.heightLocation += MainController.blockLength;
 		
 		return false;
 	}
 
-	private Direction getUsedShape() {
+	private void moveWidth(Direction direction) {
+		for(int i = this.widthLocation; i < this.widthLocation + this.sizeX; i += MainController.blockLength) {
+			for(int j = this.heightLocation; j < this.heightLocation + this.sizeY; j += MainController.blockLength) {
+				if(isUsed[i / MainController.blockLength][(j / MainController.blockLength)]) {
+					this.widthLocation += (direction == Direction.right ? (MainController.blockLength) : 0);
+					return;
+				}
+				else if(isUsed[i / MainController.blockLength][(j / MainController.blockLength)] || (i / MainController.blockLength) + 1 == 20) {
+					this.widthLocation += (direction == Direction.left ? (MainController.blockLength) * -1 : 0);
+					return;
+				}
+				
+			}
+		}
+		this.widthLocation += (direction == Direction.left ? (MainController.blockLength) * -1 : direction == Direction.right ? (MainController.blockLength) : 0);
+	}
+
+	private boolean getIsDown() {
 		for(int i = this.widthLocation; i < this.widthLocation + this.sizeX; i += MainController.blockLength) {
 			for(int j = this.heightLocation; j < this.heightLocation + this.sizeY; j += MainController.blockLength) {
 				if(isUsed[(i / MainController.blockLength) + 1][(j / MainController.blockLength) + 1]) {
 					this.addUser(this.widthLocation, this.sizeX, this.heightLocation, this.sizeY);
-					return Direction.cannotDown;
+					return true;
 				}
-				else if(isUsed[i / MainController.blockLength][(j / MainController.blockLength)]) return Direction.cannotLeft;
-				else if(isUsed[i / MainController.blockLength][(j / MainController.blockLength)] || (i / MainController.blockLength) + 1 == 20) return Direction.cannotRight;
-				
-				
 			}
 		}
-		return Direction.nothing;
+		return false;
 	}
 
 
